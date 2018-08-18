a952872628ffa2c1f246adbdf296f1d0ffc88191
agnipanda <pandaagni@outlook.com>
2018-08-18 21:13:33 +0530

    removed default attribute

--- models/model.js
+++ models/model.js
@@ -5,40 +5,40 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 var generalSchema = new Schema({
-  name : { type : String, required : true, default : ''},
-  username : { type : String, required : true, default : ''},
-  email : { type : String , required : true, default : ''},
-  contact : { type : String , required : true, default : ''},
-  address : { type : String , required : true, default : ''},
+  name : { type : String, required : true},
+  username : { type : String, required : true},
+  email : { type : String , required : true},
+  contact : { type : String , required : true},
+  address : { type : String , required : true},
   credit : { type : Number , default : 0}
   });
 var collectorSchema = new Schema({
-  name : { type : String, required : true, default : ''},
-  username : { type : String, required : true, default : ''},
-  email : { type : String , required : true, default : ''},
-  contact : { type : String , required : true, default : ''},
-  address : { type : String , required : true, default : ''},
+  name : { type : String, required : true},
+  username : { type : String, required : true},
+  email : { type : String , required : true},
+  contact : { type : String , required : true},
+  address : { type : String , required : true},
   });
 var workerSchema = new Schema({
-  name : { type : String, required : true, default : ''},
-  username : { type : String, required : true, default : ''},
-  email : { type : String , required : true, default : ''},
-  contact : { type : String , required : true, default : ''},
-  address : { type : String , required : true, default : ''},
+  name : { type : String, required : true},
+  username : { type : String, required : true},
+  email : { type : String , required : true},
+  contact : { type : String , required : true},
+  address : { type : String , required : true},
   });
 var industrySchema = new Schema({
-  name : { type : String, required : true, default : ''},
-  username : { type : String, required : true, default : ''},
-  email : { type : String , required : true, default : ''},
-  contact : { type : String , required : true, default : ''},
-  address : { type : String , required : true, default : ''},
+  name : { type : String, required : true},
+  username : { type : String, required : true},
+  email : { type : String , required : true},
+  contact : { type : String , required : true},
+  address : { type : String , required : true},
   });
 var buyerSchema = new Schema({
-  name : { type : String, required : true, default : ''},
-  username : { type : String, required : true, default : ''},
-  email : { type : String , required : true, default : ''},
-  contact : { type : String , required : true, default : ''},
-  address : { type : String , required : true, default : ''},
+  name : { type : String, required : true},
+  username : { type : String, required : true},
+  email : { type : String , required : true},
+  contact : { type : String , required : true},
+  address : { type : String , required : true},
 });
 
 var generalModel = mongoose.model('general', generalSchema);
